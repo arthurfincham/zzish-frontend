fa7f6e08e89433e1fcdf65ac5af672c0e00fc6f4
Arthur Fincham <arthurfincham@me.com>
2022-02-02 13:35:51 +0000

    add query state for SearchBox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
+import SearchBox from './components/SearchBox';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  const [query, updateQuery] = useState();
+
+  const handleTyping = (e) => {
+    updateQuery(e);
+  };
+
+  return (
+    <div className='App'>
+      <SearchBox query={query} handleTyping={handleTyping} />
+    </div>
+  );
 }
 
 export default App;
