a13b0959a870ae1304a068b2b7ebbefd0ea122f1
Arthur Fincham <arthurfincham@me.com>
2022-02-02 13:52:16 +0000

    add API call file

---
+++ src/API.js
@@ -0,0 +1,10 @@
+export const getData = async (query, page, updateGiphs) => {
+  const res = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${
+      process.env.REACT_APP_GIPHY_KEY
+    }&limit=10&q=${query}&offset=${page * 10 - 10}`
+  );
+  const response = await res.json();
+  console.log(response.data);
+  updateGiphs(response.data);
+};
